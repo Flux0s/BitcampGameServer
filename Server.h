27b17938344c37fc66b5e2ecc536951ca2c95758
Jason <Flux0s@users.noreply.github.com>
2017-04-13 20:40:26 -0400

    Comments for Server.h *****Warning***** Potential synchronization issues

--- Server.h
+++ Server.h
@@ -23,22 +23,55 @@
 
 class Server {
 public:
+
+	/*
+	 * Server
+	 * Default server constructor (unused)
+	 */
 	Server();
 
+	/*
+	 * Server
+	 * Constructor - creates a socket with the given number of clients and port
+	 */
 	Server(int, char *);
 
+	/*
+	 * spawn
+	 * Returns a thread that listens for incoming clients on the server's socket
+	 */
 	std::thread spawn();
 
+	/*
+	 * killServer
+	 * Ends the server and game safely (closes the socket)
+	 */
 	void killServer();
 
+	/*
+	 * ~Server
+	 * Deconstructs the server
+	 */
 	~Server();
 
 private:
 
+	/*
+	 * createSocket
+	 * creates a socket, called by the constructor
+	 */
 	int createSocket(struct addrinfo);
 
+	/*
+	 * getClientNum
+	 * Returns the index of the client + 1 or -1 if the client has never connected before
+	 */
 	int getClientNum(Client, Client *, int);
 
+	/*
+	 * run
+	 * The method that runs when a thread is created on the server (listens for new clients and handles client requests)
+	 */
 	void run();
 
 	int const m_MAXCLIENTS;
